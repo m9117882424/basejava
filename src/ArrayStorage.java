cfb8f81d8bbee2e318b05fa4d3b57d1e708fd606
m9117882424 <m9117882424@gmail.com>
2022-09-12 20:36:36 +0300

    HW1 refactoring done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,10 +6,6 @@ public class ArrayStorage {
     private int countOfResume = 0;
 
     void clear() {
-        //1-й вариант
-        //storage = new Resume[10000];
-
-        //2-й вариант
         for (int i = 0; i < countOfResume; i++) {
             storage[i] = null;
         }
@@ -19,12 +15,6 @@ public class ArrayStorage {
     void save(Resume r) {
         storage[countOfResume] = r;
         countOfResume++;
-//        for (int i = 0; i < storage.length; i++) {
-//            if (storage[i] == null) {
-//                storage[i] = r;
-//                return;
-//            }
-//        }
     }
 
     Resume get(String uuid) {
@@ -39,12 +29,13 @@ public class ArrayStorage {
     void delete(String uuid) {
         for (int i = 0; i < countOfResume; i++) {
             if (storage[i].toString().equals(uuid)) {
-                for (int j = i; j < countOfResume; j++) {
+                for (int j = i; j < countOfResume - 1; j++) {
                     storage[j] = storage[j + 1];
                 }
                 countOfResume--;
+                storage[countOfResume] = null;
+                return;
             }
-            return;
         }
     }
 
@@ -52,7 +43,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] allResumes = new Resume[size()];
+        Resume[] allResumes = new Resume[countOfResume];
         for (int i = 0; i < countOfResume; i++) {
             allResumes[i] = storage[i];
         }
